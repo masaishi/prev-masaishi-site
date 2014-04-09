ab39ce10be4d4a0e9445b226727b99c63b747054
masaishi <masaishi@champierre.com>
2014-04-09 19:10:22 +0900

    Update randscratch.js

--- randscratch/randscratch.js
+++ randscratch/randscratch.js
@@ -32,29 +32,28 @@ function getReq() {
     
 var req = getReq();
 try {
-	    req.open("GET", url2, false);
-	    req.send("");
-	} catch (e) {
-	    success = false;
-	    error_msg = "Error: " + e;
-	}; 
+    req.open("GET", url2, false);
+    req.send("");
+} catch (e) {
+    success = false;
+    error_msg = "Error: " + e;
+}; 
 kennsaku = req.status;
-
-while (kennsaku == 0) {
+while (kennsaku == 404) {
 	rand =  Math.floor( Math.random() * 30000000);
 	url2 = 'http://scratch.mit.edu/projects/embed/' + rand + '/?autostart=false';
 
 	var req = getReq();
-
-	try {
-	    req.open("GET", url2, false);
-	    req.send("");
-	} catch (e) {
-	    success = false;
-	    error_msg = "Error: " + e;
-	};  
-	kennsaku = req.status;   
+    try {
+        req.open("GET", url2, false);
+        req.send("");
+    } catch (e) {
+        success = false;
+        error_msg = "Error: " + e;
+    }; 
+    kennsaku = req.status;  
 };	
+
 
 text = '<iframe allowtransparency="true" width="485" height="402" src="' + url2 +'" frameborder="0" allowfullscreen></iframe><br>';
 url = '<a href="'+'http://scratch.mit.edu/projects/' + rand + '/">' + "このプロジェクトについてもっと知る" + '</a>' ;
@@ -66,7 +65,7 @@ function kurikku(){
 
 
 document.write('<table bordercolor="#ffffff" width="525"><tr><th bgcolor="#999999"><font color="#000000">')
-document.write('<a href="http://masaishi.tumblr.com/post/80512565688/scratch">randscratch</a><br><p>リロードすると作品が変ります。</p></th></tr><tr><th bgcolor="#999999"><font color="#000000">')
+document.write('<a href="http://masaishi.tumblr.com/post/80512565688/scratch">randscratch</a><br><p>リロードすると作品が変ります</p></th></tr><tr><th bgcolor="#999999"><font color="#000000">')
 document.write(text);
 document.write(url);
 document.write('</font></th></tr></table>')
